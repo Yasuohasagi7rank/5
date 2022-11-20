4412486540cc6c820cc71270875c5adcdfbfe945
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 00:57:59 +0300

    Add files via upload

---
+++ 4.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Введите первое число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+                Console.WriteLine("Число {0} больше чем {1}", a, b);
+            else
+                Console.WriteLine("Число {0} больше чем {1}", b, a);
+        }
+    }
+}
